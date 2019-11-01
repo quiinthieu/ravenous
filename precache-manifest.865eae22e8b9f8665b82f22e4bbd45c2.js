3ee4f53c7474f36780d877f40f37ff70d0445e88
Uyen Thieu <44323463+quiinthieu@users.noreply.github.com>
2019-11-01 17:36:17 +0700

    Updates

---
+++ precache-manifest.865eae22e8b9f8665b82f22e4bbd45c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "123154a4ec7f01f86ad2b2b5d7ea3332",
+    "url": "/ravenous/index.html"
+  },
+  {
+    "revision": "03e267d52982686063f3",
+    "url": "/ravenous/static/css/main.0fb6ceaf.chunk.css"
+  },
+  {
+    "revision": "50d8b5279f3b11a6e01b",
+    "url": "/ravenous/static/js/2.1cff0ab6.chunk.js"
+  },
+  {
+    "revision": "03e267d52982686063f3",
+    "url": "/ravenous/static/js/main.2f7e0f3d.chunk.js"
+  },
+  {
+    "revision": "70f65fd0f4c5223995f0",
+    "url": "/ravenous/static/js/runtime-main.471f18e8.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
